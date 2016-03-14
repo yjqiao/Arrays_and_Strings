a9119fd88f737e31959e137ce5c97f73b1563c50
Qiao Yijun-B55243 <B55243@freescale.com>
2016-03-14 03:08:02 -0500

    Created the array files

---
+++ arrays.cpp
@@ -0,0 +1,19 @@
+// this program shall include ch 1 problems.
+
+#include <iostream>
+#include <strings>
+
+
+// Determines if the input array has all-unique characters.
+// Assume we only have the 26 letters.
+int isunique(input_string){
+
+
+}
+
+
+int main <void>{
+
+}
+
+
---
+++ arrays.h
@@ -0,0 +1,8 @@
+// header file for array.c 
+
+
+#include <iostream>
+#include <strings>
+
+
+
